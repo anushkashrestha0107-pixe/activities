93cbba68d24418131be45d503da26d3ea2ec7ccc
anushkashrestha0107 <anushka.shrestha0107@gmail.com>
2026-04-15 16:46:55 +0545

    website-2

---
+++ lesson 33/lesson 34/activity1.py
@@ -0,0 +1,11 @@
+# Print a sentence
+print("Hello Everyone!")
+
+# Print a Number
+print(4)
+
+# Print both simultaneously
+print("The number being printed is", 4)
+
+# Print an empty line before/after the sentence
+print("\nI am Penguin ")  
